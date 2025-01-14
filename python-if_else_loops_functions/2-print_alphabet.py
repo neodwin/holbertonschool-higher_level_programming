da538245f063bc024b1214a9525813d7884d6a49
neodwin <neo60x@hotmail.com>
2025-01-14 16:06:33 +0100

    fix print alphabet

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-alphabet = 26
-res = ""
-for idx in range(97, 97 + alphabet):
-       res = res + chr(idx) 
-print(str(res))
+alphabet = ""
+for alpha in range(97, 123):
+    alphabet += chr(alpha)
+print("{}".format(alphabet), end="")
