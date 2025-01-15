40499a27c6e454939625c60ef8ac54c22a61b855
neodwin <neo60x@hotmail.com>
2025-01-15 10:02:05 +0100

    Add 7-islower.py & 8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for let in str:
+        if (ord(let) >= ord('a') and ord(let) <= ord('z')):
+            let = chr(ord(let) - 32)
+        print("{}".format(let), end="")
+    print("")
