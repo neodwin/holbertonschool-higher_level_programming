d6438bcbddeea6449af92f09a2c93705b584a1f0
neodwin <neo60x@hotmail.com>
2025-02-18 16:40:25 +0100

    fix task_4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -3,9 +3,14 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 users = {
-    "jane": {"username": "jane", "name": "Jane", "age": 28,
+    "jane": {"username": "jane",
+             "name": "Jane",
+             "age": 28,
              "city": "Los Angeles"},
-    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
+    "john": {"username": "john",
+             "name": "John",
+             "age": 30,
+             "city": "New York"}
 }
 
 
