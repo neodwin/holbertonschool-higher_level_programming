d65f4c4f9ddc230758945a9cc4b7739bcbd9040e
neodwin <neo60x@hotmail.com>
2025-03-04 17:27:48 +0100

    Fix script 12 & 16

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,4 @@
--- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+-- Script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
