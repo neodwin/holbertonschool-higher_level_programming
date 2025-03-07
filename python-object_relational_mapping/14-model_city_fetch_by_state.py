ad3fe7772571368c315401dac206497fa951473c
neodwin <neo60x@hotmail.com>
2025-03-07 09:50:11 +0100

    Add & fix task 14 & city model

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,29 +4,33 @@ Script that prints all City objects from the database hbtn_0e_14_usa
 """
 
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    MYSQL_USERNAME = sys.argv[1]
-    MYSQL_PASSWORD = sys.argv[2]
-    DATABASE_NAME = sys.argv[3]
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
 
-    engine = create_engine(
-        f'mysql+mysqldb://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@localhost/'
-        f'{DATABASE_NAME}',
-        pool_pre_ping=True
-    )
-    Base.metadata.create_all(engine)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
-    cities = session.query(City).join(State).order_by(City.id).all()
+    query = session.query(City, State).join(State).order_by(City.id).all()
 
-    for city in cities:
-        print(f"{city.state.name}: ({city.id}) {city.name}")
+    for city, state in query:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -4,7 +4,6 @@ Module class of city.
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 from model_state import Base
 
 
@@ -13,9 +12,6 @@ class City(Base):
     City class.
     """
     __tablename__ = 'cities'
-
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-    state = relationship("State", back_populates="cities")
