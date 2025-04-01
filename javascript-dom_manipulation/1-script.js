8b25cc7fd958ebd3c077ab1bd975b8a89cd5c75d
neodwin <neo60x@hotmail.com>
2025-04-01 16:55:52 +0200

    Add script 1 & 2

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,8 @@
+const header_red = document.getElementById('red_header')
+
+header_red.addEventListener('click', function() {
+
+  const header = document.querySelector('header')
+
+  header.style.color = '#FF0000'
+});
---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,8 @@
+const header_red = document.getElementById('red_header');
+
+header_red.addEventListener('click', function() {
+
+  const header = document.querySelector('header')
+
+  header.classList.add('red')
+});
